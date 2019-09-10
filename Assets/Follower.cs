693e93d8e15a9e4b26849e59a93e3f1cb254938f
CarAle17 <carla0000666@gmail.com>
2019-09-09 21:54:46 -0500

    asd

---
+++ Assets/Follower.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follower : MonoBehaviour
+{
+    Vector3 desired =  Vector3.zero;
+    Vector3 velocity =  Vector3.zero;
+    Vector3 steer =  Vector3.zero;
+
+   public float MAX_SPEED = 5;
+   public float MAX_STEER = 1;
+
+   Transform target;
+
+  
+
+   void Update(){
+       
+       
+
+       target = GameObject.Find("Target").transform;      
+       Vector3 player = transform.position;
+       
+      
+
+       desired = -(target.position - transform.position).normalized * MAX_SPEED;
+       steer = Vector3.ClampMagnitude(desired - velocity, MAX_STEER);
+       
+       Vector3 screenPosition =  Camera.main.WorldToScreenPoint(transform.position);
+
+       if( player.x >= -1  &&  player.x <= 1 && player.y >= -1 && player.y <= 1) 
+       {
+           print("u win");
+            Destroy(this.gameObject);   
+        }
+       if( player.x > screenPosition.x || player.y > screenPosition.y) {
+           print("u lose");
+           Destroy(this.gameObject); 
+           }
+
+
+       velocity+=desired*Time.deltaTime;
+       transform.position += velocity * Time.deltaTime;
+   }   
+}
---
+++ Assets/Target.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+   // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePosition =  Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.Scale(new Vector3(1,1,0));
+        transform.position = mousePosition;
+    }
+
+
+    //VersionTWO
+    /*
+    using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+	float tx = 0, ty = 0;
+
+	void Update()
+	{
+		tx += 0.01f;
+		ty += 0.01f;
+		float x = -5 + 10 * Mathf.PerlinNoise(tx, 0);
+		float y = -5 + 10 * Mathf.PerlinNoise(0, ty);
+		transform.position = new Vector3(x, y, 0);
+	}
+}
+*/
+}
